540e5dc4199163c9ec39e369977e9ba0cc971349
SmartBoy84 <barfie@gabba.ga>
2023-01-04 23:02:18 +0800

    oops

--- tweak/layout/usr/libexec/receiver.sh
+++ tweak/layout/usr/libexec/receiver.sh
@@ -1,7 +1,7 @@
 # put whatever you want in here
 
-if [[ $# -eq 0 ]] ; then
+if [[ $1 -eq 0 ]] ; then
     # do something if deactivate signal was sent
-elif [[ $# -eq 1 ]] ; then
+elif [[ $1 -eq 1 ]] ; then
     # do something if activate signal was sent
 fi
