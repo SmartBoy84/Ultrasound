4c678a169c4a026786451094f6b5b9d2869d1683
SmartBoy84 <barfie@gabba.ga>
2023-01-04 22:59:29 +0800

    template + instructions

---
+++ tweak/README.md
@@ -0,0 +1,3 @@
+This is a simple tweak that runs a script when a connected client send either an activate or deactivate signal
+
+Script can be found /usr/libexec/receiver.sh
--- tweak/layout/usr/libexec/receiver.sh
+++ tweak/layout/usr/libexec/receiver.sh
@@ -1,1 +1,7 @@
-touch /User/Downloads/tim
+# put whatever you want in here
+
+if [[ $# -eq 0 ]] ; then
+    # do something if deactivate signal was sent
+elif [[ $# -eq 1 ]] ; then
+    # do something if activate signal was sent
+fi
