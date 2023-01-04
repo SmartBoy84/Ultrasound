07c497ab0af536401d7dff38d1c7b06cbca176b1
SmartBoy84 <barfie@gabba.ga>
2023-01-04 23:44:33 +0800

    fix errors, I hate bash

--- tweak/layout/usr/libexec/receiver.sh
+++ tweak/layout/usr/libexec/receiver.sh
@@ -3,10 +3,12 @@ if [ ! -z "$1" ]; then
     
     if [[ "$1" -eq "0" ]] ; then
         # do something if deactivate signal was sent
+        echo "[goceiver] DEACTIVATE"
     fi
     
     if [[ "$1" -eq "1" ]] ; then
         # do something if activate signal was sent
+        echo "[goceiver] ACTIVATE"
         activator send libactivator.system.vibrate
     fi
     
